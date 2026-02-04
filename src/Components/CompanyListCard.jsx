8e82e64bce1bcd4dee7876fcbd124af1b0b65a64
Hridya Singhal <157908813+HridyaSinghal@users.noreply.github.com>
2026-02-04 11:08:40 +0000

    change alignment

--- src/Components/CompanyListCard.jsx
+++ src/Components/CompanyListCard.jsx
@@ -6,12 +6,12 @@ const CompanyListCard = ({ company }) => {
     <>
 
     <div className='border w-230 h-27 m-4 rounded-sm shadow-md flex wrap hover:shadow-lg hover:border-blue-500 items-center hover:scale-105 transition-all duration-300 relative'>
-        <div className='font-semibold text-2xl w-40 ml-10 flex justify-center items-center flex-wrap'>
+        <div className='font-semibold text-2xl w-40 ml-10 flex items-center flex-wrap'>
         <h2>{company.companyName}</h2>
         </div>
-        <div className='text-lg flex ml-10 gap-6 flex justify-center items-center '>
+        <div className='text-lg flex justify-center gap-6 flex items-center '>
             <p>Role: {company.role}</p>
-            <p>CTC: {company.ctc}</p>
+            <p>CTC: {company.salary}</p>
             <p>Location: {company.location}</p>  
             <div className='m-auto absolute right-10 '>
             <Link to={`/student/student-dashboard/company-list/job-description/${company.id}`} state={{ company }} className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 cursor-pointer'>Apply</Link>    
